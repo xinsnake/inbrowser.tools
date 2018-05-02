1739891ca54af7670df1121bb74a3fd1be323a2f
Xinyun Zhou <me@xyzhou.com>
2018-05-03 00:52:08 +1000

    adding aws publish

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,26 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    aws: grunt.file.readJSON("aws-keys.json"),
+    aws_s3: {
+      options: {
+        accessKeyId: "<%= aws.AWSAccessKeyId %>",
+        secretAccessKey: "<%= aws.AWSSecretKey %>",
+        region: "us-east-1",
+        uploadConcurrency: 5,
+        downloadConcurrency: 5
+      },
+      production: {
+        options: {
+          bucket: "inbrowser.tools",
+          params: {
+            ContentEncoding: "gzip"
+          }
+        },
+        files: [
+          { expand: true, cwd: "dist/", src: ["**"], dest: "/" }
+        ]
+      }
+    },
     pug: {
       compile: {
         options: {
@@ -38,10 +59,12 @@ module.exports = function(grunt) {
     }
   })
 
-  grunt.loadNpmTasks("grunt-contrib-pug")
-  grunt.loadNpmTasks("grunt-contrib-copy")
-  grunt.loadNpmTasks("grunt-contrib-watch")
+  grunt.loadNpmTasks("grunt-aws-s3")
   grunt.loadNpmTasks("grunt-contrib-clean")
+  grunt.loadNpmTasks("grunt-contrib-copy")
+  grunt.loadNpmTasks("grunt-contrib-pug")
+  grunt.loadNpmTasks("grunt-contrib-watch")
 
   grunt.registerTask("default", ["clean", "pug", "copy", "watch"])
+  grunt.registerTask("deploy", ["clean", "pug", "copy", "aws_s3"])
 }
